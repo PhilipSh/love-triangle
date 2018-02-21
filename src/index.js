20662b8547128c97990ebcfa418a132e57771344
Philip Shpen <fil3120025@gmail.com>
2018-02-22 01:18:06 +0300

    task2

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count = 0;
+  var firstLover,secondLover,thirdLover;
+
+  for (var i = 0; i < preferences.length; i++) {
+      firstLover = preferences[i];
+      secondLover = preferences[firstLover -1 ];
+      thirdLover = preferences[secondLover -1 ];
+      if(thirdLover== i){
+        count++;
+    }
+  }
+  return Math.floor(count/3);
 };
